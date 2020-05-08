29e75d359a6533cfd0b1fa04d2f7a31cf8b21f64
lenaavenerovna <lenaavenerovna@gmail.com>
2020-05-08 21:47:02 +0300

    First Change

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+#
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status='published')
